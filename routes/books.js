7014596a17fa35c2c3b2b7d283999826f96a95d0
Patrick Dullano <PWDullano@gmail.com>
2016-02-08 11:46:40 -0700

    successfully getting book data in console.log

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
+
+function Books(){
+return knex('books');
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('books/index');
+  Books().then(function(results){
+    res.render('books/index', {books:results})
+    console.log(results);
+  })
 });
 
 module.exports = router;
