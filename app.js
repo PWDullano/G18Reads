0f6d288dbb24add12696233beacb29a7c7c43fc5
Patrick Dullano <PWDullano@gmail.com>
2016-02-08 11:07:58 -0700

    added views for books and authors, also added routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var books = require('./routes/books');
+var authors = require('./routes/authors');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/books', books);
+app.use('/authors', authors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
