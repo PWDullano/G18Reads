a97c2d33b98a71d0363cf386a3f8996b43e85562
Patrick Dullano <PWDullano@gmail.com>
2016-02-08 21:49:15 -0700

    author data populating author index page

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
+
+function Authors(){
+  return knex('author');
+}
 
 /* GET home page. */
 router.get('/authors', function(req, res, next) {
-  res.render('authors/index');
+  Authors().select().then(function(authors){
+    res.render('authors/index', {authors:authors})
+  })
 });
 
 module.exports = router;
